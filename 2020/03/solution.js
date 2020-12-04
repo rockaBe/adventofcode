4e552b1ac09dd08af6585812701f559d76b39528
Bernd Naehler <bnaehler@babbel.com>
2020-12-04 13:32:05 +0100

    2020-03: use (x,y) order and add comment

--- 2020/03/solution.js
+++ 2020/03/solution.js
@@ -8,18 +8,19 @@ const spinUpMatrix = () => getData().then(
 const tree = '#';
 
 const treeHunt = (steps) =>
-  spinUpMatrix().then((data) =>
+  spinUpMatrix().then((matrix) =>
     steps.map(([r, d]) => {
-      const width = data[0].length;
+      const width = matrix[0].length;
       let x = 0;
       let y = 0;
       let hits = 0;
 
-      while (y < data.length - 1) {
+      while (y < matrix.length - 1) {
         x = x + r;
         y = y + d;
-        const modulo = x % width;
-        if (data[y][modulo] === tree) {
+        // to repeat the matrix to the right
+        // simply start from left again x % width
+        if (matrix[y][x % width] === tree) {
           hits++;
         }
       };
@@ -28,11 +29,15 @@ const treeHunt = (steps) =>
     })
   );
 
-const part1 = [[3,1]];
-treeHunt(part1).then(trees => console.log(trees));
+const main = () => {
+  const part1 = [[3,1]];
+  treeHunt(part1).then(trees => console.log(trees));
 
-const part2 = [[1,1], [3,1], [5,1], [7,1], [1,2]];
-treeHunt(part2).then(trees => {
-  console.log(trees.reduce((m, cnt) => m * cnt, 1));
-});
+  const part2 = [[1,1], [3,1], [5,1], [7,1], [1,2]];
+  treeHunt(part2).then(trees => {
+    console.log(trees.reduce((m, cnt) => m * cnt, 1));
+  });
+}
+
+main();
 
