528b4ea647a225fd90b20044e9c55f5706816d53
Bernd Naehler <bnaehler@babbel.com>
2020-12-06 22:22:07 +0100

    2020-06: add solution for part2

--- 2020/06/solution.js
+++ 2020/06/solution.js
@@ -4,13 +4,25 @@ const onlyUnique = (value, index, self) => self.indexOf(value) === index;
 const groupsInput = () => getData().then(
   (data) => data.split('\n\n'));
 
-const getGroupAnswers = (groupInput) =>
+// part1 getAddedGroupAnswers
+const getAddedGroupAnswers = (groupInput) =>
   groupInput.split('')
     .filter(el => el !== '\n')
     .filter(onlyUnique).length;
 
-groupsInput().then((groups) =>
-  console.log(groups.map(getGroupAnswers)
-  .reduce((memo, el) => memo = memo + el, 0)));
+const getGroupAgreedYes = (groupInput) => {
+  const clearedInput = groupInput.replace(/\n$/m, '');
+  const groupLength = clearedInput.split('\n').length;
+  const answers = clearedInput.split('').filter(el => el !== '\n');
+  const uniqueAnswers = answers.filter(onlyUnique);
+  return uniqueAnswers.map((answer) =>
+    answers.filter(a => a === answer).length === groupLength ? 1 : 0
+  ).reduce((memo, a) => memo + a, 0);
+}
 
-
+groupsInput().then((groups) => {
+  console.log(groups.map(getAddedGroupAnswers)
+    .reduce((memo, el) => memo = memo + el, 0));
+  console.log(groups.map(getGroupAgreedYes)
+    .reduce((memo, el) => memo = memo + el, 0));
+});
