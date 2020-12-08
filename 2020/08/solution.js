3707c7a50c918ce88f0ac8665c248662dea79c22
Bernd Naehler <bnaehler@babbel.com>
2020-12-08 20:45:14 +0100

    2020-08: restructure code

--- 2020/08/solution.js
+++ 2020/08/solution.js
@@ -3,39 +3,44 @@ const getData = require('../helpers');
 
 const numberify = (str) => {
   const split = str.split('');
-  if (split[0] === '+') {
-    delete split[0]
-  }
+  if (split[0] === '+') { delete split[0] };
   return parseInt(split.join(''), 10);
-}
+};
+
+const parseInput = async () =>
+  getData().then((data) =>
+    data.split('\n').filter(n => n)
+      .map(val => splitCommandAndVal(val)));
 
 const splitCommandAndVal = (el) => {
   const [cmd, sVal] = el.split(' ');
   return { cmd, val: numberify(sVal) };
+};
+
+const runCommands = ({state, parsed}) => {
+  while (true) {
+    if (state.visited.includes(state.index)) { break; }
+    const { cmd, val } = parsed[state.index]
+    state.visited.push(state.index);
+    switch (cmd) {
+      case 'acc':
+        state.index++;
+        state.accumulator += val;
+        break;
+      case 'jmp':
+        state.index += val;
+        break;
+      case 'nop':
+        state.index++;
+    }
+  };
+  return state;
 }
 
-const part1 = async () =>
-  getData().then((data) => {
-    const input = data.split('\n').filter(n => n);
-    const parsed = input.map(val => splitCommandAndVal(val));
-    let state = { accumulator: 0, index: 0, visited: []};
-    while (true) {
-      if (state.visited.includes(state.index)) { break; }
-      const { cmd, val } = parsed[state.index]
-      state.visited.push(state.index);
-      switch (cmd) {
-        case 'acc':
-          state.index++;
-          state.accumulator += val;
-          break;
-        case 'jmp':
-          state.index += val;
-          break;
-        case 'nop':
-          state.index++;
-      }
-    };
-    return state;
-});
+const part1 = async () => {
+  const parsed = await parseInput();
+  const state = { accumulator: 0, index: 0, visited: []};
+  return runCommands({state, parsed})
+};
 
-part1().then((result => console.log(result)));
+part1().then(result => console.log(result))
