504e4ccf40e82e708997ba88f7fd2e6bdbf0fc3c
Bernd Naehler <bnaehler@babbel.com>
2020-12-09 23:14:57 +0100

    2020-09: add solution for part 2, based on outcome of part 1

--- 2020/09/solution.js
+++ 2020/09/solution.js
@@ -15,20 +15,47 @@ const sumAll = (arr) => {
   return sums;
 };
 
-const iterate = (arr, preamble=25) => {
+const sumArr = (idcs, arr) =>
+  idcs.reduce((memo, idx) => memo + arr[idx], 0);
+
+const sumOfMinAndMax = (arr) =>
+  Math.max(...arr) + Math.min(...arr);
+
+const sumUpTo = (arr, target) => {
+  let summandIndices = [];
+  for (let i=0; i<arr.length; i++) {
+    summandIndices.push(i);
+    const interSum = sumArr(summandIndices, arr);
+    // success case
+    if (summandIndices.length > 1 && interSum === target) { break; }
+    // reset index and list of indices
+    if (interSum > target) {
+      i = summandIndices[0];
+      summandIndices = [];
+    }
+  }
+  return [summandIndices, sumOfMinAndMax(summandIndices.map(i => arr[i]))];
+}
+
+const findInvalid = (arr, preamble=25) => {
   let index = 0;
   let candidate;
   while (index < arr.length-1) {
     const target = arr[preamble];
-    if (!sumAll(arr.slice(index, preamble)).includes(target)) {
-      candidate = target
+    const options = arr.slice(index, preamble);
+    if (!sumAll(options).includes(target)) {
+      candidate = target;
       break;
     }
     index++; preamble++;
   }
   return candidate;
-}
+};
 
-parseInput().then(data => console.log(iterate(data, 25)));
+parseInput().then(data => {
+  const part1 = findInvalid(data, 25);
+  console.log('Solution part 1: ', part1);
+  console.log('Solution part 2: ', sumUpTo(data, part1));
+});
 
 
