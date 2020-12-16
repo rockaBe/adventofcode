0c1c79ba061210352b3f701f1f77a6207e9586ad
Bernd Naehler <bnaehler@babbel.com>
2020-12-16 20:35:16 +0100

    2020-13: working for small numbers, but not for input

--- 2020/13/solution.js
+++ 2020/13/solution.js
@@ -3,14 +3,7 @@ const getData = require('../helpers');
 const parseInput = async (sample = null) =>
   getData(sample).then((data) =>
     data.split('\n')
-      .filter(n => n)
-      .reduce((memo, el, idx) => {
-        idx === 0
-          ? memo.arrival = parseInt(el)
-          : memo.departures = el.split(',').map(e => parseInt(e)).filter(n=>n);
-        return memo;
-      }, {})
-    );
+      .filter(n => n));
 
 // receives
 // earliest: time of earliest departure (aka arrival)
@@ -22,6 +15,7 @@ const calculateBusDeparture = (earliest, id) => Math.ceil(earliest / id) * id;
 // @busId: <Integer>
 // @nextDeparture: <Integer>
 const pickNextBus = (arr) =>
+
   arr.reduce((memo, {busId, nextDeparture}) => {
     if (nextDeparture < memo.nextDeparture) {
       memo.busId = busId;
@@ -31,10 +25,55 @@ const pickNextBus = (arr) =>
   }, { busId: null, nextDeparture: Infinity });
 
 parseInput().then(data => {
-  const calculatedInput = data.departures.map(dTime => ({
+  const part1Data = data.reduce((memo, el, idx) => {
+    idx === 0
+      ? memo.arrival = parseInt(el)
+      : memo.departures = el.split(',').map(e => parseInt(e)).filter(n => n);
+    return memo;
+  }, {});
+  const calculatedInput = part1Data.departures.map(dTime => ({
     busId: dTime,
-    nextDeparture: calculateBusDeparture(data.arrival, dTime)
+    nextDeparture: calculateBusDeparture(part1Data.arrival, dTime)
   }));
+
   const result = pickNextBus(calculatedInput);
-  console.log(`Part 1: busId: ${result.busId}, departure: ${result.nextDeparture} and result: ${result.busId * (result.nextDeparture - data.arrival)}`);
+  console.log(`Part 1: busId: ${result.busId}, departure: ${result.nextDeparture} and result: ${result.busId * (result.nextDeparture - part1Data.arrival)}`);
+});
+
+const generateModuloLogic = (arrOfBusAndDelay) => {
+  let t = null;
+  const conditions = [];
+  arrOfBusAndDelay.forEach(({ busId, increment }, idx) => {
+    if (idx === 0) { t = busId; return; }
+    else {
+      conditions.push((arg) => {
+        console.log('arg in condition func: ', arg);
+       return (arg + increment) % busId === 0
+      });
+    }
+  });
+  return { t, conditions };
+}
+
+parseInput().then(data => {
+  const part2Data = data[1].split(',').map((e, idx) => {
+    if (e !== 'x') {
+      return { busId: parseInt(e), increment: idx };
+    }
+  }).filter(n => n);
+
+  const { t, conditions } = generateModuloLogic(part2Data);
+  const theT = parseInt(t)
+  let timestamp = theT;
+  while (timestamp < Infinity) {
+    if (conditions.every(el => {
+      console.log('timestamp: ', timestamp);
+      return el(timestamp);
+    })) {
+      break
+    } else {
+      timestamp += theT;
+    }
+  }
+  console.log(`Part 2: timestamp is: ${timestamp}`);
 });
